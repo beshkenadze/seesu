35548d98aca2a4ef6919fa0169bebe290e09be28
Aleksandr Koss <koss@octolab.ru>
2009-10-17 15:58:26 +0700

    Added updates action

--- seesu.rb
+++ seesu.rb
@@ -1,5 +1,21 @@
+require 'rubygems'
 require 'sinatra'
+require 'json'
+
+mime :json, 'appliaction/json'
 
 get '/' do
   "Bro, you wanna susi?! Ha?!"
+end
+
+get '/updates/:current_version' do
+  content_type :json
+  
+  { 
+    :latest_version => '0.0',
+    :message => {
+      :title => 'Hi!',
+      :body => 'Hello cruel world!'
+    }
+  }.to_json
 end
