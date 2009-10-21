bab19da2c25738bad594154504cd1ea90ed71897
Aleksandr Koss <koss@octolab.ru>
2009-10-21 22:18:39 +0700

    Added DataMapper support

--- seesu.rb
+++ seesu.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'json'
+require 'dm-core'
 
 set :app_file, __FILE__
 set :root, File.dirname(__FILE__)
@@ -8,11 +9,23 @@ set :views, 'views'
 
 mime :json, 'appliaction/json'
 
-get '/' do
-  "Bro, you wanna susi?! Ha?!"
+DataMapper.setup :default, ENV['DATABASE_URL'] || 'sqlite3://my.db'
+
+class UsageInfo
+  include DataMapper::Resource
+
+  property :hash,    String
+  property :version, String
+  property :when,    DateTime
 end
 
-get '/updates/:current_version' do
+
+get '/' do
+  "Hi, I'm Seesu server. Nice to meet you!"
+end
+
+post '/update' do
+
   content_type :json
   
   { 
