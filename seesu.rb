fff066031eeb392b9929bd1865851aa237b9832d
Aleksandr Koss <koss@octolab.ru>
2009-10-21 22:36:35 +0700

    Added basic json schema

--- seesu.rb
+++ seesu.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'sinatra'
 require 'json'
 require 'dm-core'
+require 'do_sqlite3'
 
 set :app_file, __FILE__
 set :root, File.dirname(__FILE__)
@@ -14,11 +15,12 @@ DataMapper.setup :default, ENV['DATABASE_URL'] || 'sqlite3://my.db'
 class UsageInfo
   include DataMapper::Resource
 
-  property :hash,    String
-  property :version, String
-  property :when,    DateTime
+  property :hash,        String
+  property :version,     String
+  property :when,        DateTime
+  property :demention_x, Integer
+  property :demention_y, Integer
 end
-
 
 get '/' do
   "Hi, I'm Seesu server. Nice to meet you!"
@@ -28,11 +30,43 @@ post '/update' do
 
   content_type :json
   
-  { 
-    :latest_version => '0.0',
-    :message => {
-      :title => 'Hi!',
-      :body => 'Hello cruel world!'
+  {
+    latest_version : {
+      number: '0.2',
+      link:   'seesu.wgt'
+    }, 
+    
+    inviters: {
+      yodapunk : {
+        count: 0,
+        link : "http://vk.com/reg....",
+        select: true
+      },
+      
+      kossnocorp: {
+        count: 12,
+        link : 'http://vk.com/reg....',
+        select: false
+      }, 
+      
+      porqz: {
+        count: 0,
+        link : "http://vk.com/reg....",
+        select: false
+      },
+      
+      elv1s : {
+        count: 0,
+        link : "http://vk.com/reg....",
+        select: false
+      }    
+    },
+    
+    promo: {
+      text: "blabla",
+      lang: "en",
+      number: 22,
+      until: "2009.10.25"
     }
   }.to_json
 end
