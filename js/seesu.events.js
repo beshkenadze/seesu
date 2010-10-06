1d9fd9e49f2e32a4476de63e68a7d53a01cc9d59
Gleb Arestov <gr1b0k@gmail.com>
2010-10-06 10:18:45 +0400

    no events

--- js/seesu.events.js
+++ js/seesu.events.js
@@ -294,34 +294,6 @@ $(function(){
 })
 
 
-// Ready? Steady? Go!
-if(!!(a.canPlayType && a.canPlayType('audio/mpeg;').replace(/no/, ''))){
-	seesu.player.workable.push('html5_audio');
-} else{
-	soundManager = new SoundManager();
-	if (soundManager){
-		soundManager.url = 'http://seesu.me/swf/';
-		soundManager.flashVersion = 9;
-		soundManager.useFlashBlock = true;
-		soundManager.debugMode = false;
-		soundManager.wmode = 'transparent';
-		soundManager.useHighPerformance = true;
-		soundManager.onready(function() {
-		  if (soundManager.supported()) {
-			console.log('sm2 in widget ok');
-			seesu.player.workable.push('sm2');
-			try_to_use_iframe_sm2p(true);
-		  } else {
-		  		console.log('sm2 in widget notok')
-		  		try_to_use_iframe_sm2p();
-	
-		  }
-		});
-	}
-}
-
-
-
 $(function() {
 	seesu.ui.player_holder = $('<div class="player-holder"></div>')
 		.prepend(seesu.player.controls.track_progress_total)
